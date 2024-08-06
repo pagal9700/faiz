9547c671ace82ede66f52edd363550defb9e1848
🥀आतंकवादी की प्रेम🥀 कहानी नहीं होती🥀 <faizansari90791545@gmail.com>
2024-08-06 22:50:01 +0530

    Update music.js

--- Priyansh/commands/music.js
+++ Priyansh/commands/music.js
@@ -6,7 +6,7 @@ const contentDisposition = require("content-disposition");
 
 module.exports = {
   config: {
-    name: "music",
+    name: "sing",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
